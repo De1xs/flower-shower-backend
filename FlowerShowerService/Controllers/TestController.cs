68fe79e7954a749f672775d146605345eb0459fb
DESKTOP-ET90EA5\Deividas T <deivistilindis@gmail.com>
2022-04-29 11:45:13 +0300

    Initial commit with test controller

---
+++ FlowerShowerService/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlowerShowerService.Controllers
+{
+    public class TestController : Controller
+    {
+        [HttpGet("test")]
+        public IActionResult TestEndpoint()
+        {
+            return Ok("Hello world");
+        }
+    }
+}
