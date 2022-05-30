8d2e3150e48ce13c44baac37b27fe30de66c2fd6
DESKTOP-ET90EA5\Deividas T <deivistilindis@gmail.com>
2022-05-30 20:24:18 +0300

    little fix here and t here

--- FlowerShowerService/Data/Entities/User.cs
+++ FlowerShowerService/Data/Entities/User.cs
@@ -5,5 +5,4 @@ public class User
     public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public List<Order> Orders { get; set; } = new();
 }
